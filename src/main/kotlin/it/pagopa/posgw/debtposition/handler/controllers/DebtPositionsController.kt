16ebde22d7448afcc579476aa0e6b7d9682d4e51
Ivan Ferrando <ivan.ferrando@pagopa.it>
2026-09-15 10:04:38 +0200

    feat: [CHK-4950] add openapi + code generation (#2)
    * feat: enable debtposition-handler openapi generation task
    
    * feat: add openapi + DebtPositionsController
    
    * chore: spotless
    
    * feat: add POST /invalidations api
    
    * feat: add openapi quality check workflow
    
    * fix(openapi): mark API server as internal
    
    * fix(openapi): add 401 Unauthorized response schema

---
+++ src/main/kotlin/it/pagopa/posgw/debtposition/handler/controllers/DebtPositionsController.kt
@@ -0,0 +1,25 @@
+package it.pagopa.posgw.debtposition.handler.controllers
+
+import it.pagopa.generated.posgw.debtposition.handler.api.DebtPositionsApi
+import it.pagopa.generated.posgw.debtposition.handler.model.DebtPositionRequestDto
+import it.pagopa.generated.posgw.debtposition.handler.model.DebtPositionResponseDto
+import java.util.UUID
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DebtPositionsController : DebtPositionsApi {
+    override suspend fun createDebtPosition(
+        xCorrelationId: UUID,
+        debtPositionRequestDto: DebtPositionRequestDto
+    ): ResponseEntity<DebtPositionResponseDto> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun invalidateDebtPosition(
+        xCorrelationId: UUID,
+        iupd: String
+    ): ResponseEntity<Unit> {
+        TODO("Not yet implemented")
+    }
+}
